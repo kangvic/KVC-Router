612426ffacfd67509834ead937d753284c1f43e5
PejuangBerkah <31729118+kangvic@users.noreply.github.com>
2024-02-27 10:49:03 +0800

    Add files via upload

---
+++ colorvc.js
@@ -0,0 +1,125 @@
+		
+      window.onload = function(){
+      var hargavc = document.getElementById("hargaku").innerHTML;
+      console.log(hargavc);
+
+      if (hargavc == '2.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#e62129";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#e62129";
+
+
+        }       
+
+      }
+      if (hargavc == '3.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#05c800";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#05c800";
+        }       
+
+      } 
+      if (hargavc == '5.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#004eff";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#004eff";
+        }       
+
+      }   
+      if (hargavc == '10.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#197fbf";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#197fbf";
+        }       
+
+      }
+    	if (hargavc == '15.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#228B22";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#228B22";
+
+        }       
+
+      }     
+      if (hargavc == '20.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#C23777";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#C23777";
+
+        }       
+
+      }  
+      if (hargavc == '25.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#008000";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#008000";
+
+        }       
+
+      }  
+      if (hargavc == '30.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#FF00FF";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#FF00FF";
+
+        }       
+
+      } 
+      if (hargavc == '50.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#E60C00";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#E60C00";
+
+        }       
+
+      } 
+      if (hargavc == '100.000') {
+      const nodeList = document.getElementsByClassName("warnaharga");
+      const nodeFile = document.getElementsByClassName("warnalist")
+      for (let i = 0; i < nodeList.length; i++) {
+          nodeList[i].style.color= "#6d8726";
+        }
+      for (let i = 0; i < nodeFile.length; i++) {
+          nodeFile[i].style.background= "#6d8726";
+
+        }
+
+      }
+
+    }
